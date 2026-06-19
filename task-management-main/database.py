3bd84dbc53d3fae6d9c9aa47d0563b816d0e039a
Zayaan Scher G <71695956+zayaan03@users.noreply.github.com>
2026-06-19 20:18:07 +0500

    Update database.py

--- task-management-main/database.py
+++ task-management-main/database.py
@@ -9,16 +9,18 @@ class Database():
     This class creates a single connection with Supabase client (Database)
     which is used in all CRUD functions
     '''
-    _instance = None
-
-    def __new__(cls):
-        if cls._instance == None:
-            cls._instance = super().__new__(cls)
+    def __init__(self):
+        try:
             url = st.secrets["SUPABASE_URL"]
             key = st.secrets["SUPABASE_KEY"]
-            if not url or not key:
-                raise ValueError('SUPABASE_URL and SUPABASE_KEY must be set in .env')
-            cls._instance.client: Client = create_client(url, key)
-        return cls._instance
-    def get_client(self) -> Client:
+        except:
+            url = os.getenv('SUPABASE_URL')
+            key = os.getenv('SUPABASE_KEY')
+
+        if not url or not key:
+            raise ValueError("Supabase credentials not found")
+
+        self.client = create_client(url, key)
+
+    def get_client(self):
         return self.client
